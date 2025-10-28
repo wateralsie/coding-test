c73ffebc6b7e869156f826a4dbdc10ab89741e31
wateralsie <minee1913@gmail.com>
2025-10-28 10:52:06 +0900

    1. 문자 찾기

--- 인프런/src/GetLetterCount.java
+++ 인프런/src/GetLetterCount.java
@@ -1,5 +1,25 @@
 // 문자 찾기
 
-public class FindLetter {
-    public 
+public class GetLetterCount {
+    public int mySolution(String str, char t) {
+        int answer = 0;
+        // 대문자 or 소문자로 통일
+        str = str.toLowerCase();
+        t = Character.toLowerCase(t);
+        // 문자열에서 특정 문자의 개수 탐색
+        // 1. 인덱스 기준 탐색
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == t) {
+//                answer++;
+//            }
+//        }
+        // 2. for-each
+        for (char x : str.toCharArray()) {  // 문자 배열 생성
+            if (x == t) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
 }
--- 인프런/src/Main.java
+++ 인프런/src/Main.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // 백준
-        // static 메소드에서 메소드 호출하려면 클래스 객체 생성 필수
-        // 호출하려는 메소드가 static일 시 필요없음
+        // static 메소드에서 인스턴스 메소드 호출하려면 클래스 객체 생성 필수
+        // 호출하려는 메소드가 static일 시 별도 객체 생성 필요없음
 
         // Main T = new Main();
         GetLetterCount glc = new GetLetterCount();
+
         Scanner kb = new Scanner(System.in);
-        return ;
+        String str = kb.next();
+        char c = kb.next().charAt(0);   // 문자 입력받는 방법
+
+        System.out.println(glc.mySolution(str, c));
     }
 }
